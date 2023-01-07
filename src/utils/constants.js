c6af20d2f9512a2b747c6a4ef53ee2b52b888b80
matheuspedrow <matheuspedrow@gmail.com>
2023-01-07 18:48:02 -0300

    iniciando constants

---
+++ src/utils/constants.js
@@ -0,0 +1,39 @@
+export const ARTIGOS_A = [
+  {
+    artigo: 1,
+    titulo: 'Ameaça',
+    pena: 7,
+    multa: 40000,
+  },
+  {
+    artigo: 2,
+    titulo: 'Associacção criminosa',
+    pena: 10,
+    multa: 50000,
+  },
+  {
+    artigo: 3,
+    titulo: 'Corrida Ilegal',
+    pena: 10,
+    multa: 10000,
+  },
+  {
+    artigo: 4,
+    titulo: 'Conspiraçao',
+    pena: 5,
+    multa: 2500,
+  },
+  {
+    artigo: 5,
+    titulo: 'Comportamento indisciplinar',
+    pena: 5,
+    multa: 50000,
+  },
+  {
+    artigo: 6,
+    titulo: 'Dano ao patrimonio publico/privado',
+    pena: 10,
+    multa: 90000,
+  },
+];
+
