2d9118d527afb3f5e4fa651f050a84115cb3ce7b
matheuspedrow <matheuspedrow@gmail.com>
2023-01-08 16:04:56 -0300

    update code

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,39 +1,50 @@
 export const ARTIGOS_A = [
   {
-    artigo: 1,
-    titulo: 'Ameaça',
-    pena: 7,
-    multa: 40000,
-  },
-  {
-    artigo: 2,
-    titulo: 'Associacção criminosa',
-    pena: 10,
-    multa: 50000,
-  },
-  {
-    artigo: 3,
-    titulo: 'Corrida Ilegal',
-    pena: 10,
-    multa: 10000,
-  },
-  {
-    artigo: 4,
-    titulo: 'Conspiraçao',
-    pena: 5,
-    multa: 2500,
-  },
-  {
-    artigo: 5,
-    titulo: 'Comportamento indisciplinar',
-    pena: 5,
-    multa: 50000,
-  },
-  {
-    artigo: 6,
-    titulo: 'Dano ao patrimonio publico/privado',
-    pena: 10,
-    multa: 90000,
+    titulo: 'Crimes de Classe 1',
+    artigos: [
+      {
+        nome: 'Art. 01 - Ameaça',
+        penal: 7,
+        multa: 40000
+      },
+      {
+        nome: 'Art. 02 - Associação criminosa',
+        penal: 10,
+        multa: 50000
+      },
+      {
+        nome: 'Art. 03 - Corrida ilegal',
+        penal: 10,
+        multa: 10000
+      },
+      {
+        nome: 'Art. 04 - Conspiração',
+        penal: 5,
+        multa: 2500
+      },
+      {
+        nome: 'Art. 05 - Comportamento indisciplinar',
+        penal: 5,
+        multa: 50000
+      },
+      {
+        nome: 'Art. 06 - Dano ao patrimônio público/privado',
+        penal: 10,
+        multa: 90000
+      },
+      {
+        nome: 'Art. 07 - Desobediência',
+        penal: 5,
+        multa: 50000
+      },
+      {
+        nome: 'Art. 08 - Desobediência',
+        penal: 5,
+        multa: 50000
+      },
+      
+    ],
   },
 ];
 
+    
