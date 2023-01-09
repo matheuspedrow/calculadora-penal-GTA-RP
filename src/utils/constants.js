1907a4f75e99b946db77ef6b323820ddc28c82c9
matheuspedrow <matheuspedrow@gmail.com>
2023-01-09 14:28:47 -0300

    penal = pena

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,142 +4,142 @@ export const ARTIGOS_A = [
     artigos: [
       {
         nome: 'Art. 01 - Ameaça',
-        penal: 7,
+        pena: 7,
         multa: 40000
       },
       {
         nome: 'Art. 02 - Associação criminosa',
-        penal: 10,
+        pena: 10,
         multa: 50000
       },
       {
         nome: 'Art. 03 - Corrida ilegal',
-        penal: 10,
+        pena: 10,
         multa: 10000
       },
       {
         nome: 'Art. 04 - Conspiração',
-        penal: 5,
+        pena: 5,
         multa: 2500
       },
       {
         nome: 'Art. 05 - Comportamento indisciplinar',
-        penal: 5,
+        pena: 5,
         multa: 50000
       },
       {
         nome: 'Art. 06 - Dano ao patrimônio público/privado',
-        penal: 10,
+        pena: 10,
         multa: 90000
       },
       {
         nome: 'Art. 07 - Desacato',
-        penal: 30,
+        pena: 30,
         multa: 200000
       },
       {
         nome: 'Art. 08 - Desobediência',
-        penal: 15,
+        pena: 15,
         multa: 150000
       },
       {
         nome: 'Art. 09 - Posse dinheiro sujo',
-        penal: 30,
+        pena: 30,
         multa: 50000
       },
       {
         nome: 'Art. 10 - Posse de drogas',
-        penal: 15,
+        pena: 15,
         multa: 50000
       },
       {
         nome: 'Art. 11 - Falsidade ideológica',
-        penal: 10,
+        pena: 10,
         multa: 100000
       },
       {
         nome: 'Art. 12 - Falso Testemunho',
-        penal: 10,
+        pena: 10,
         multa: 100000
       },
       {
         nome: 'Art. 13 - Tentativa de Fuga',
-        penal: 5,
+        pena: 5,
         multa: 50000
       },
       {
         nome: 'Art. 14 - Estelionato',
-        penal: 5,
+        pena: 5,
         multa: 50000
       },
       {
         nome: 'Art. 15 - Furto',
-        penal: 20,
+        pena: 20,
         multa: 80000
       },
       {
         nome: 'Art. 16 - Injúria',
-        penal: 5,
+        pena: 5,
         multa: 60000
       },
       {
         nome: 'Art. 17 - Difamação',
-        penal: 10,
+        pena: 10,
         multa: 60000
       },
       {
         nome: 'Art. 18 - Calúnia',
-        penal: 10,
+        pena: 10,
         multa: 60000
       },
       {
         nome: 'Art. 19 - Objetos Roubados',
-        penal: 10,
+        pena: 10,
         multa: 20500
       },
       {
         nome: 'Art. 20 - Ocultação Facial',
-        penal: 10,
+        pena: 10,
         multa: 70000
       },
       {
         nome: 'Art. 21 - Omissão de Socorro',
-        penal: 10,
+        pena: 10,
         multa: 10000
       },
       {
         nome: 'Art. 22 - Perseguição',
-        penal: 8,
+        pena: 8,
         multa: 50000
       },
       {
         nome: 'Art. 23 - Perturbação da Tranquilidade',
-        penal: 5,
+        pena: 5,
         multa: 50000
       },
       {
         nome: 'Art. 24 - Produtos Ilícitos',
-        penal: 8,
+        pena: 8,
         multa: 0
       },
       {
         nome: 'Art. 25 - Resistência',
-        penal: 10,
+        pena: 10,
         multa: 70000
       },
       {
         nome: 'Art. 26 - Trajes Proibidos',
-        penal: 0,
+        pena: 0,
         multa: 50000
       },
       {
         nome: 'Art. 27 - Suborno',
-        penal: 5,
+        pena: 5,
         multa: 20500
       },
       {
         nome: 'Art. 28 - Obstrução da Justiça',
-        penal: 10,
+        pena: 10,
         multa: 30000
       },      
     ],
@@ -152,82 +152,82 @@ export const ARTIGOS_B = [
     artigos: [
       {
         nome: 'Art. 1 - Extorsão',
-        penal: 10,
+        pena: 10,
         multa: 50000
       },
       {
         nome: 'Art. 2 - Invasão',
-        penal: 5,
+        pena: 5,
         multa: 20500
       },
       {
         nome: 'Art. 3 - Assalto a Mao Armada',
-        penal: 10,
+        pena: 10,
         multa: 50000
       },
       {
         nome: 'Art. 4 - Lesão Corporal',
-        penal: 25,
+        pena: 25,
         multa: 50000
       },
       {
         nome: 'Art. 5 - Lesão Corporal Culposa',
-        penal: 10,
+        pena: 10,
         multa: 30000
       },
       {
         nome: 'Art. 6 - Porte de Arma Ilegal Arma Leve',
-        penal: 15,
+        pena: 15,
         multa: 100000
       },
       {
         nome: 'Art. 7 - Receptação',
-        penal: 10,
+        pena: 10,
         multa: 50000
       },
       {
         nome: 'Art. 8 - Receptação Culposa',
-        penal: 5,
+        pena: 5,
         multa: 20500
       },
       {
         nome: 'Art. 9 - Receptação',
-        penal: 10,
+        pena: 10,
         multa: 50000
       },
       {
         nome: 'Art. 10 - Latrocínio',
-        penal: 60,
+        pena: 60,
         multa: 300000
       },
       {
         nome: 'Art. 11 - Roubo',
-        penal: 30,
+        pena: 30,
         multa: 150000
       },
       {
         nome: 'Art. 12 - Tentativa de Roubo/Furto',
-        penal: 10,
+        pena: 10,
         multa: 50000
       },
       {
         nome: 'Art. 13 - Tráfico de Drogas',
-        penal: 20,
+        pena: 20,
         multa: 100000
       },
       {
         nome: 'Art. 14 - Tráfico de Armas Leves',
-        penal: 25,
+        pena: 25,
         multa: 250000
       },
       {
         nome: 'Art. 14 - Violencia Arbitrária',
-        penal: 15,
+        pena: 15,
         multa: 100000
       },
       {
         nome: 'Art. 15 - Homicidio Culposo',
-        penal: 45,
+        pena: 45,
         multa: 100000
       },
     ]
@@ -240,37 +240,37 @@ export const ARTIGOS_C = [
     artigos: [
       {
         nome: 'Art. 1 - Corrupção',
-        penal: 100,
+        pena: 100,
         multa: 300000
       },
       {
         nome: 'Art. 2 - Tentativa / Homicídio',
-        penal: 70,
+        pena: 70,
         multa: 150000
       },
       {
         nome: 'Art. 3 - Tentativa / Homicidio Oficial',
-        penal: 80,
+        pena: 80,
         multa: 200000
       },
       {
         nome: 'Art. 4 - Homicídio Qualificado',
-        penal: 100,
+        pena: 100,
         multa: 300000
       },
       {
         nome: 'Art. 5 - Sequestro',
-        penal: 30,
+        pena: 30,
         multa: 100000
       },
       {
         nome: 'Art. 6 - Porte de Arma Ilegal Pesada',
-        penal: 30,
+        pena: 30,
         multa: 200000
       },
       {
         nome: 'Art. 7 - Tráfico de Armas Pesadas',
-        penal: 50,
+        pena: 50,
         multa: 500000
       },
     ]
@@ -283,31 +283,29 @@ export const ARTIGOS_D = [
     artigos: [
       {
         nome: 'Art. 1 - Alta Velocidade',
-        penal: 0,
+        pena: 0,
         multa: 20500
       },
       {
         nome: 'Art. 2 - Direção Perigosa',
-        penal: 0,
+        pena: 0,
         multa: 20500
       },
       {
         nome: 'Art. 3 - Destruição de Patrimônio Público ou Privado',
-        penal: 0,
+        pena: 0,
         multa: 20500
       },
       {
         nome: 'Art. 4 - Estacionamento Proibido',
-        penal: 0,
+        pena: 0,
         multa: 20500
       },
       {
         nome: 'Art. 5 - Transitar em Faixa Oposta',
-        penal: 0,
+        pena: 0,
         multa: 50000
       },
     ]
   }
 ];
-
-    
